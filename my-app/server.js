1475bf682e970d3a4d3b548535eb09524fd5e30f
Ravi Tharun <tr565003@gmail.com>
2026-05-25 11:24:40 +0530

    upd

--- my-app/server.js
+++ my-app/server.js
@@ -3,21 +3,23 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: "https://systemdesgin.netlify.app"
+}));
 
 // ✅ API ROUTE
 app.get("/api/user", (req, res) => {
-  res.json({
-    success: true,
-    message: "User route working",
-    data: {
-      name: "Tharun",
-      role: "Developer"
-    }
-  });
+    res.json({
+        success: true,
+        message: "User route working",
+        data: {
+            name: "Tharun",
+            role: "Developer"
+        }
+    });
 });
 
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+    console.log("Server running on http://localhost:5000");
 });
